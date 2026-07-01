919bef2d87f184ab7c51826387a1e39a74e68024
smruti-alegaonkar <smriti1312004@gmail.com>
2026-07-01 22:35:03 +0530

    Merge

---
+++ merge.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1; // last valid element in nums1
+        int j = n - 1; // last element in nums2
+        int k = m + n - 1; // last position in nums1
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // copy remaining elements of nums2 (if any)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
